9a591e58b27c1296907a53815804d29d71193240
Dan Novac <danynovac@gmail.com>
2020-08-28 00:13:08 +0300

    added type on entity

---
+++ src/main/java/com/daninovac/batch/jobs/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.daninovac.batch.jobs.utils;
+
+/**
+ * @author dan on 27/08/2020
+ * @project batch-jobs
+ */
+public class Constants {
+
+}
---
+++ src/main/java/com/daninovac/batch/jobs/web/dto/FileTypeEnum.java
@@ -0,0 +1,8 @@
+package com.daninovac.batch.jobs.web.dto;
+
+/**
+ * @author dan on 27/08/2020
+ * @project batch-jobs
+ */
+public enum FileTypeEnum {
+}

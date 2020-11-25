cefcc932739772e8a33e01fe6c8b0c3f412d9171
Dan Novac <danynovac@gmail.com>
2020-11-25 22:22:50 +0200

    - Updated MongoRepo - Works to save in the default "test" mongo db

--- src/main/java/com/daninovac/batch/jobs/batch/writer/XmlWriter.java
+++ src/main/java/com/daninovac/batch/jobs/batch/writer/XmlWriter.java
@@ -1,8 +1,11 @@
 package com.daninovac.batch.jobs.batch.writer;
 
+import com.daninovac.batch.jobs.entity.CsvDataDocument;
 import com.daninovac.batch.jobs.entity.XmlData;
+import com.daninovac.batch.jobs.repository.CsvDataRepository;
 import com.daninovac.batch.jobs.repository.XmlDataRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -17,9 +20,23 @@ public class XmlWriter implements ItemWriter<XmlData> {
 
   private final XmlDataRepository xmlDataRepository;
 
+  private final CsvDataRepository csvDataRepository;
+
   @Override
   public void write(List<? extends XmlData> data) {
     log.info("Writing data chunk of {} from XML import to database...", data.size());
     xmlDataRepository.saveAll(data);
+
+    //todo migrate and leave only this code
+    List<CsvDataDocument> csvDataDocuments = data.stream().map(xmlData -> {
+      CsvDataDocument doc = new CsvDataDocument();
+      doc.setId(xmlData.getId().toString());
+      doc.setFilename(xmlData.getFilename());
+      doc.setType(xmlData.getType());
+      doc.setProperties(xmlData.getProperties());
+      return doc;
+    }).collect(Collectors.toList());
+
+    csvDataRepository.saveAll(csvDataDocuments);
   }
 }
---
+++ src/main/java/com/daninovac/batch/jobs/repository/CsvDataRepository.java
@@ -0,0 +1,14 @@
+package com.daninovac.batch.jobs.repository;
+
+import com.daninovac.batch.jobs.entity.CsvDataDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Dan Novac on 25/11/2020
+ * @project batch-jobs
+ */
+@Repository
+public interface CsvDataRepository extends MongoRepository<CsvDataDocument, String> {
+
+}
